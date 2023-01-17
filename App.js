dfd9f394a178f933c55c30ed01f8ccb0d94d63b9
Matheus Anthony <thehellc4t@gmail.com>
2023-01-17 11:13:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Accelerometer } from "expo-sensors";
+import * as React from "react";
+
+import { StyleSheet, View, Button } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
-  });
-  const [subscription, setSubscription] = useState(null);
-
-  const _slow = () => Accelerometer.setUpdateInterval(400);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
-
-  const _subscribe = () => {
-    setSubscription(Accelerometer.addListener(setData));
+  const speak = () => {
+    const thingToSay = "qualquer coisa teste";
+    Speech.speak(thingToSay);
   };
-
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
-
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Accelerometer: (in gs where 1g = 9.81 m/s^2)
-      </Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={subscription ? _unsubscribe : _subscribe}
-          style={styles.button}
-        >
-          <Text>{subscription ? "On" : "Off"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={_slow}
-          style={[styles.button, styles.middleButton]}
-        >
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
+      <Button title="Pressione para ouvir algumas palavras" onPress={speak} />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
